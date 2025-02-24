e91a34e4332743afa4b438185986af9b17505494
abhishekbhanot <abhishekbhanot756@gmail.com>
2025-02-24 18:49:43 +0530

    added nextjs practice

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +9,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body >
+      
+      <header className="bg-gray-800 text-white p-4 text-center">
+              We will be releasing version 1.1.0 soon...
+            </header>
+      
+      {children}
+      
+      <footer className="bg-gray-800 text-white text-center p-4 mt-8">
+              © 2025 Sari App | MountainKid AI
+            </footer>
+      </body>
     </html>
   );
 }
